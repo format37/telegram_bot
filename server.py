181aee11745b6f6821c3386a3fbc1fea3a6d695c
Alex <format37@gmail.com>
2020-06-18 00:05:54 +0300

    modified:   server.py

--- server.py
+++ server.py
@@ -13,9 +13,9 @@ SCRIPT_PATH	= '/home/format37_gmail_com/projects/telegram_bot_server/'
 #load from parallel paths
 import sys
 sys.path.append('/home/format37_gmail_com/projects/f37t1')
-from f37t1 import test
+import f37t1
 
-test()
+#test()
 
 WEBHOOK_HOST = 'www.scriptlab.net'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
@@ -34,11 +34,14 @@ WEBHOOK_SSL_PRIV = SCRIPT_PATH+'webhook_pkey.pem'  # Path to the ssl private key
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
-
+'''
 with open(SCRIPT_PATH+'token.key','r') as file:
 	API_TOKEN=file.read().replace('\n', '')
 	file.close()
 bot = telebot.TeleBot(API_TOKEN)
+'''
+bots=[]
+bots.append( f37t1_bot )
 
 WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
 WEBHOOK_URL_PATH = "/{}/".format(API_TOKEN)
@@ -47,38 +50,34 @@ app = web.Application()
 
 # Process webhook calls
 async def handle(request):
-    print('request',request)
-    if request.match_info.get('token') == bot.token:
-        request_body_dict = await request.json()
-        update = telebot.types.Update.de_json(request_body_dict)
-        bot.process_new_updates([update])
-        return web.Response()
-    else:
-        return web.Response(status=403)
+    #print('request',request)
+	for bot in bots:
+		if request.match_info.get('token') == bot.token:
+			request_body_dict = await request.json()
+			update = telebot.types.Update.de_json(request_body_dict)
+			bot.process_new_updates([update])
+			return web.Response()
+#		else:
+	return web.Response(status=403)
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message,"t1")
-
-# Handle '/user'
-@bot.message_handler(commands=['user'])
-def send_user(message):
-            bot.reply_to(message,   str(message.from_user.id) )	
+	bots[0].reply_to(message,"t1")
 	
 # Handle '/group'
 @bot.message_handler(commands=['group'])
 def send_user(message):
-            bot.reply_to(message,   str(message.chat.id) )
+	bots[0].reply_to(message,   str(message.chat.id) )
 		
 
 app.router.add_post('/{token}/', handle)
 
 # Remove webhook, it fails sometimes the set if there is a previous webhook
-bot.remove_webhook()
+bots[0].remove_webhook()
 
 # Set webhook
-wh_res = bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+wh_res = bots[0].set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
                 certificate=open(WEBHOOK_SSL_CERT, 'r'))
 print('webhook set',wh_res)
 print(WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
