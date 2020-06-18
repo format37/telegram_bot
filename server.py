b20128b0141a5e3afe776d2bcd873254ffbd0896
Alex <format37@gmail.com>
2020-06-18 18:34:52 +0300

    modified:   server.py

--- server.py
+++ server.py
@@ -7,11 +7,9 @@ import ssl
 from aiohttp import web
 import telebot
 import asyncio
+import sys
 
 SCRIPT_PATH	= '/home/format37_gmail_com/projects/telegram_bot_server/'
-
-#load from parallel paths
-import sys
 
 WEBHOOK_HOST = 'www.scriptlab.net'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
@@ -34,10 +32,45 @@ telebot.logger.setLevel(logging.INFO)
 app = web.Application()
 bots	= []
 
+# === === === f37t1 ++
+
 sys.path.append('/home/format37_gmail_com/projects/f37t1')
 from f37t1 import bot as f37t1_bot
 bot_t1	= f37t1_bot(WEBHOOK_HOST,WEBHOOK_PORT,WEBHOOK_SSL_CERT)
 bots.append( bot_t1 )
+
+# Handle '/start' and '/help'
+@bot_t1.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+	bot_t1.reply_to(message,"t1")	
+
+# Handle '/group'
+@bot_t1.message_handler(commands=['group'])
+def send_user(message):
+	bot_t1.reply_to(message,   str(message.chat.id) )
+
+# === === === f37t1 --
+
+
+# === === === calcubot ++
+
+sys.path.append('/home/format37_gmail_com/projects/calcubot')
+from calcubot import calcubot_init
+calcubot	= calcubot_init(WEBHOOK_HOST,WEBHOOK_PORT,WEBHOOK_SSL_CERT)
+bots.append( calcubot )
+
+# Handle '/start' and '/help'
+@calcubot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+	calcubot.reply_to(message,"t1")	
+
+# Handle '/group'
+@calcubot.message_handler(commands=['group'])
+def send_user(message):
+	calcubot.reply_to(message,   str(message.chat.id) )
+
+# === === === calcubot --
+
 
 # Process webhook calls
 async def handle(request):
@@ -50,25 +83,8 @@ async def handle(request):
 	#else:
 	return web.Response(status=403)
 
-# Handle '/start' and '/help'
-@bot_t1.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-	bot_t1.reply_to(message,"t1")
-	
-# Handle '/group'
-@bot_t1.message_handler(commands=['group'])
-def send_user(message):
-	bot_t1.reply_to(message,   str(message.chat.id) )
-
 app.router.add_post('/{token}/', handle)
 
-'''
-# Set webhook
-wh_res = bot_t1.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
-                certificate=open(WEBHOOK_SSL_CERT, 'r'))
-print('bot_t1 webhook set',wh_res)
-print(WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
-'''
 # Build ssl context
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
