c863824bcff53b1befa43369d865bdd76525392a
Alex <format37@gmail.com>
2020-06-18 21:05:13 +0300

    modified:   server.py

--- server.py
+++ server.py
@@ -59,6 +59,13 @@ from calcubot import calcubot_init, calcubot_eval
 calcubot	= calcubot_init(WEBHOOK_HOST,WEBHOOK_PORT,WEBHOOK_SSL_CERT)
 bots.append( calcubot )
 
+@bot.inline_handler(lambda query: query.query == 'text')
+def query_text(inline_query):
+    if message.from_user.id==106129214:
+		r = types.InlineQueryResultArticle('1', 'Result', types.InputTextMessageContent('Result message.'))
+        r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('Result message2.'))
+        bot.answer_inline_query(inline_query.id, [r, r2])
+
 @calcubot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
 	calcubot.reply_to(message,"t1")
