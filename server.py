d75634180ca8ad6476376323da7e956ea1539a23
Alex <format37@gmail.com>
2020-07-31 12:27:01 +0300

    modified:   server.py

--- server.py
+++ server.py
@@ -34,6 +34,44 @@ telebot.logger.setLevel(logging.INFO)
 app = web.Application()
 bots	= []
 
+def default_bot_init(WEBHOOK_HOST,WEBHOOK_PORT,WEBHOOK_SSL_CERT, SCRIPT_PATH):
+
+	with open(SCRIPT_PATH+'token.key','r') as file:
+		API_TOKEN=file.read().replace('\n', '')
+		file.close()
+	bot = telebot.TeleBot(API_TOKEN)
+
+	WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
+	WEBHOOK_URL_PATH = "/{}/".format(API_TOKEN)
+
+	# Remove webhook, it fails sometimes the set if there is a previous webhook
+	bot.remove_webhook()
+
+	# Set webhook
+	wh_res = bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,certificate=open(WEBHOOK_SSL_CERT, 'r'))
+	print('webhook set',wh_res)
+	print(WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
+
+	return bot
+
+# === === === id37bot ++
+
+try:
+	IDBOT_SCRIPT_PATH	= '/home/format37_gmail_com/projects/id37bot/'
+	#sys.path.append(IDBOT_SCRIPT_PATH)	
+	idbot	= default_bot_init(WEBHOOK_HOST,WEBHOOK_PORT,WEBHOOK_SSL_CERT,ROVER_SCRIPT_PATH)
+	bots.append( idbot )
+	
+	@idbot.message_handler(commands=['user'])
+	def idbot_user(message):
+		idbot.reply_to(message, str(message.from_user.id))
+
+except Exception as e:
+		print('idbot',str(e))
+		
+# === === === id37bot --
+		
+		
 # === === === rover ++
 
 try:
