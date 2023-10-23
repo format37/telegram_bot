ba695ee2e0ef8e29cb64f0de8d5baf7a73674c1c
Alex <format37@gmail.com>
2023-10-23 13:03:04 +0400

    modified:   server/Dockerfile 	modified:   server/server.py

--- server/server.py
+++ server/server.py
@@ -1,22 +1,23 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import ssl
-import os
-from aiohttp import web
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 import telebot
-import json
+import os
 import logging
+import ssl
 
-# init logging
+# Initialize FastAPI
+app = FastAPI()
+
+# Initialize logging
 logging.basicConfig(level=logging.INFO)
-# logging.basicConfig(level=logging.WARNING)
-logging.getLogger('aiohttp.access').setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+# Environment variables
 WEBHOOK_HOST = os.environ.get('WEBHOOK_HOST', '')
-# 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_PORT = os.environ.get('WEBHOOK_PORT', '')
+# 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 WEBHOOK_SSL_CERT = 'webhook_cert.pem'
 WEBHOOK_SSL_PRIV = 'webhook_pkey.pem'
@@ -29,80 +30,53 @@ WEBHOOK_SSL_PRIV = 'webhook_pkey.pem'
 # When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
 # with the same value in you put in WEBHOOK_HOST
 
-async def call_test(request):
-    logging.info('call_test')
-    content = "ok"
-    return web.Response(text=content, content_type="text/html")
+
+@app.get("/test")
+async def call_test():
+    logger.info('call_test')
+    return JSONResponse(content={"status": "ok"})
 
 
+# Initialize bot
 def default_bot_init(bot_token_env):
     API_TOKEN = os.environ.get(bot_token_env, '')
     bot = telebot.TeleBot(API_TOKEN)
 
-    WEBHOOK_URL_BASE = "https://{}:{}".format(
-        os.environ.get('WEBHOOK_HOST', ''),
-        os.environ.get('WEBHOOK_PORT', '')
-    )
-    WEBHOOK_URL_PATH = "/{}/".format(API_TOKEN)
-
-    # Remove webhook, it fails sometimes the set if there is a previous webhook
-    bot.remove_webhook()
-
     # Set webhook
-    logger.info('WEBHOOK_URL_BASE: %s', WEBHOOK_URL_BASE)
-    logger.info('WEBHOOK_URL_PATH: %s', WEBHOOK_URL_PATH)
-    logger.info('WEBHOOK_SSL_CERT: %s', WEBHOOK_SSL_CERT)
-    wh_res = bot.set_webhook(
-        url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH, certificate=open(WEBHOOK_SSL_CERT, 'r'))
-    # print(bot_token_env, 'webhook set', wh_res)
-    logger.info('webhook set: %s', wh_res)
-    # print(WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
+    WEBHOOK_URL_BASE = f"https://{WEBHOOK_HOST}:{WEBHOOK_PORT}"
+    WEBHOOK_URL_PATH = f"/{API_TOKEN}/"
+
+    logger.info(f'WEBHOOK_URL_BASE: {WEBHOOK_URL_BASE}')
+    logger.info(f'WEBHOOK_URL_PATH: {WEBHOOK_URL_PATH}')
+    logger.info(f'WEBHOOK_SSL_CERT: {WEBHOOK_SSL_CERT}')
+
+    bot.remove_webhook()
+    wh_res = bot.set_webhook(url=f"{WEBHOOK_URL_BASE}{WEBHOOK_URL_PATH}", certificate=open(WEBHOOK_SSL_CERT, 'r'))
+    logger.info(f'webhook set: {wh_res}')
 
     return bot
 
 
-# Process webhook calls
-async def handle(request):
-    for bot in bots:
-        if request.match_info.get('token') == bot.token:
-            request_body_dict = await request.json()
-            update = telebot.types.Update.de_json(request_body_dict)
-            bot.process_new_updates([update])
-            return web.Response()
+@app.post("/{token}/")
+async def handle(token: str, request: Request):
+    request_body_dict = await request.json()
+    update = telebot.types.Update.de_json(request_body_dict)
 
-    return web.Response(status=403)
+    if token == pantherabot.token:
+        pantherabot.process_new_updates([update])
+        return JSONResponse(content={"status": "ok"})
+    else:
+        raise HTTPException(status_code=403, detail="Invalid token")
 
 
-bots = []
+# bots = []
 
 # === @pantherabot ++
 pantherabot = default_bot_init('PANTHERABOT_TOKEN')
-bots.append(pantherabot)
+# bots.append(pantherabot)
 
-@pantherabot.message_handler(commands=['help', 'start'])
-def pantherabot_help_start(message):
-    # log user
-    logger.info('pantherabot_help_start: %s', message.chat.id)
+@pantherabot.message_handler()
+def pantherabot_message(message):
+    logger.info(f'pantherabot_message from: {message.chat.id}')
     pantherabot.reply_to(message, 'hello')
 # === @pantherabot --
-
-def main():
-    logging.info('Init')
-    app = web.Application()
-    app.router.add_post('/{token}/', handle)
-    app.router.add_route('GET', '/test', call_test)
-    # Build ssl context
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
-    logging.info('Starting')
-    # Start aiohttp server
-    web.run_app(
-        app,
-        host=WEBHOOK_LISTEN,
-        port=os.environ.get('DOCKER_PORT', ''),
-        ssl_context=context
-    )
-    
-
-if __name__ == "__main__":
-    main()
