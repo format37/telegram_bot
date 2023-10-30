fb50c9869abab30ead5b48366a5104896b2b3424
Alex <format37@gmail.com>
2023-10-30 13:58:49 +0400

    modified:   server/server.py

--- server/server.py
+++ server/server.py
@@ -91,16 +91,16 @@ def pantherabot_message(message):
             pantherabot.reply_to(message, result_message['body'])
 
         elif result_message['type'] == 'keyboard':
-            # logger.info(f'keyboard: {result_message}')
+            logger.info(f'keyboard: {result_message}')
             keyboard_dict = result_message['body']
-            # logger.info(f'keyboard_dict: {keyboard_dict}')
+            logger.info(f'keyboard_dict: {keyboard_dict}')
             keyboard = telebot.types.ReplyKeyboardMarkup(
                 row_width=keyboard_dict['row_width'], 
                 resize_keyboard=keyboard_dict['resize_keyboard']
                 )
-            # logger.info(f'keyboard: {keyboard}')
+            logger.info(f'keyboard: {keyboard}')
             for button_definition in keyboard_dict['buttons']:
-                # logger.info(f'button_definition: {button_definition}')
+                logger.info(f'button_definition: {button_definition}')
                 button = types.KeyboardButton(
                     text=button_definition['text'],
                     request_contact=button_definition['request_contact']
