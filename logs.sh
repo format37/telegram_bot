686578d52685cfa0579767f5ade12ce7a7e75f85
Alex <format37@gmail.com>
2023-11-10 15:13:24 +0400

    new file:   build.sh 	modified:   logs.sh 	renamed:    server/run.sh -> run.sh 	deleted:    server/build.sh 	deleted:    server/logs.sh

--- logs.sh
+++ logs.sh
@@ -1,5 +1,5 @@
 # get id of running container with image name
-container_id=$(sudo docker ps | grep telegram_bot_server_1 | awk '{print $1}')
+container_id=$(sudo docker ps | grep telegram_bot | awk '{print $1}')
 echo "container id: $container_id"
 # get logs
 sudo docker logs -f $container_id
--- server/build.sh
+++
@@ -1,1 +0,0 @@
-sudo docker build -t telegram_bot .
