435b53bdfd11e52b2666301bf57146e385d57975
Alex <format37@gmail.com>
2023-11-10 15:26:26 +0400

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # sudo docker run -d --restart always -p 7401:7401  partners_bot --name partners_bot
-sudo docker run --name telegram_bot --network="host" -v $(pwd)/bots.json:/server/bots.json  telegram_bot
+sudo docker run --name telegram_bot --rm --network="host" -v $(pwd)/bots.json:/server/bots.json  telegram_bot
 # sudo docker run -p 7401:7401  partners_bot
