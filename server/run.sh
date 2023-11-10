2af24774dc74cbd778c4270a58a5079b5b436095
Alex <format37@gmail.com>
2023-11-10 15:06:38 +0400

    new file:   server/build.sh 	new file:   server/logs.sh 	new file:   server/run.sh

---
+++ server/run.sh
@@ -0,0 +1,3 @@
+# sudo docker run -d --restart always -p 7401:7401  partners_bot --name partners_bot
+sudo docker run --name telegram_bot --network="host" telegram_bot
+# sudo docker run -p 7401:7401  partners_bot
