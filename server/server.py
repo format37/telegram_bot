77161fcb95db45f6a077676f3da7fac068ab87f7
Alex <format37@gmail.com>
2023-11-14 11:49:14 +0400

    modified:   server/server.py

--- server/server.py
+++ server/server.py
@@ -54,8 +54,8 @@ def default_bot_init(bot_token_env):
 
 
 def get_bot_feature_by_token(token, feature):
-    # logger.info(f'get_bot_feature_by_token. token: {token}, feature: {feature}')
-    # logger.info(f'bots: {bots}')
+    logger.info(f'get_bot_feature_by_token. token: {token}, feature: {feature}')
+    logger.info(f'bots: {bots}')
     for bot_key, bot_instance in bots.items():
         if bot_instance['TOKEN'] == token:
             return bot_instance[feature]
