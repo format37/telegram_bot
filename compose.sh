31b94a4740215d174ba045e30284f10d7810d906
Alex <format37@gmail.com>
2023-12-28 07:57:21 +0400

    deleted:    build.sh 	modified:   compose.sh 	deleted:    run.sh

--- compose.sh
+++ compose.sh
@@ -1,4 +1,4 @@
 # sudo docker-compose up --build --force-recreate -d --remove-orphans
 # sudo docker-compose up --build -d
 # echo "Compose approach is deprecated. Please, use build."
-sudo docker-compose up --build --force-recreate --remove-orphans
+sudo docker-compose up --build --force-recreate --remove-orphans -d
