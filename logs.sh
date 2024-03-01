723b1e2051a9251da067c48c45054a3b05b1ff42
Alex <format37@gmail.com>
2024-03-01 12:41:19 +0400

    modified:   logs.sh

--- logs.sh
+++ logs.sh
@@ -1,7 +1,1 @@
-# get id of running container with image name
-container_id=$(sudo docker ps | grep telegram_bot | awk '{print $1}')
-echo "container id: $container_id"
-# get logs
-sudo docker logs -f $container_id
-# connect to container
-# sudo docker exec -it $container_id /bin/bash
+sudo docker logs -f bots_server_telegram
