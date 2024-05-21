8b37e8a8b5c161e8b048504bb323163a86359167
Alex <format37@gmail.com>
2024-05-21 15:37:45 +0400

    modified:   compose.sh

--- compose.sh
+++ compose.sh
@@ -1,5 +1,2 @@
-# sudo docker-compose up --build --force-recreate -d --remove-orphans
-# sudo docker-compose up --build -d
-# echo "Compose approach is deprecated. Please, use build."
-# sudo docker-compose down -v
+sudo docker compose down -v
 sudo docker compose up --build --force-recreate --remove-orphans -d
