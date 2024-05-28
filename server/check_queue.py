ae66f78444fd357de82df839722317d28cf3a2d1
Alex <format37@gmail.com>
2024-05-28 16:57:00 +0400

    modified:   server/check_queue.py

--- server/check_queue.py
+++ server/check_queue.py
@@ -1,8 +1,12 @@
 from telebot import TeleBot
+import time
+import datetime
 
 bot = TeleBot('your_bot_token')
 
-webhook_info = bot.get_webhook_info()
-pending_update_count = webhook_info.pending_update_count
-
-print(f"Number of pending updates: {pending_update_count}")
+while (1):
+    webhook_info = bot.get_webhook_info()
+    pending_update_count = webhook_info.pending_update_count
+    datetime_prefix = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"{datetime_prefix} Number of pending updates: {pending_update_count}")
+    time.sleep(1)
