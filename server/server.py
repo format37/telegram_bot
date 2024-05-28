77b40353b067cde5dab27758c5eaf785d46fd8b3
Alex <format37@gmail.com>
2024-05-28 18:10:54 +0400

    modified:   server/server.py

--- server/server.py
+++ server/server.py
@@ -213,37 +213,37 @@ def handle_inline_query(bot, inline_query, bot_config):
         logger.error(f'Error sending inline query: {str(e)}')
         return JSONResponse(content={"status": "ok"})
 
-    from_user_id = inline_query.from_user.id
-    inline_query_id = inline_query.id
-    expression = inline_query.query
+    # from_user_id = inline_query.from_user.id
+    # inline_query_id = inline_query.id
+    # expression = inline_query.query
 
-    if result.status_code == 200:
-        try:
-            result_message = json.loads(result.text)
-            answer = result_message['body']
-            if result_message['type'] != 'inline':
-                logger.error(f'Inline: Invalid response type: {result_message["type"]}')
-                return JSONResponse(content={"status": "ok"})
-            inline_elements = []
-            for i in range(len(answer)):    
-                element = telebot.types.InlineQueryResultArticle(
-                    str(i),
-                    answer[i],
-                    telebot.types.InputTextMessageContent(answer[i]),
-                )
-                inline_elements.append(element)
+    # if result.status_code == 200:
+    #     try:
+    #         result_message = json.loads(result.text)
+    #         answer = result_message['body']
+    #         if result_message['type'] != 'inline':
+    #             logger.error(f'Inline: Invalid response type: {result_message["type"]}')
+    #             return JSONResponse(content={"status": "ok"})
+    #         inline_elements = []
+    #         for i in range(len(answer)):    
+    #             element = telebot.types.InlineQueryResultArticle(
+    #                 str(i),
+    #                 answer[i],
+    #                 telebot.types.InputTextMessageContent(answer[i]),
+    #             )
+    #             inline_elements.append(element)
             
             
-            bot.answer_inline_query(
-                inline_query_id,
-                inline_elements,
-                cache_time=0,
-                is_personal=True
-            )
-        except Exception as e:
-            logger.error(f'User: {from_user_id} Inline request: {expression}  Error processing inline query: {str(e)}')
-    else:
-        logger.error(f"[x] Failed to send inline query. Status code: {result.status_code}, Response: {result.content}")
+    #         bot.answer_inline_query(
+    #             inline_query_id,
+    #             inline_elements,
+    #             cache_time=0,
+    #             is_personal=True
+    #         )
+    #     except Exception as e:
+    #         logger.error(f'User: {from_user_id} Inline request: {expression}  Error processing inline query: {str(e)}')
+    # else:
+    #     logger.error(f"[x] Failed to send inline query. Status code: {result.status_code}, Response: {result.content}")
 
     """
     logger.info(f'### Sending inline_query_url: {inline_query_url}')
