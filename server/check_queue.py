63ed36e77a0aa11c980d81c601a6f892f2b31db3
Alex <format37@gmail.com>
2024-05-28 16:54:54 +0400

    modified:   server/check_queue.py 	modified:   server/requirements.txt

--- server/check_queue.py
+++ server/check_queue.py
@@ -1,23 +1,8 @@
-from telegram.ext import Updater
+from telebot import TeleBot
 
-# Replace with your bot token
-BOT_TOKEN = 'YOUR_BOT_TOKEN'
+bot = TeleBot('your_bot_token')
 
-updater = Updater(token=BOT_TOKEN, use_context=True)
-dispatcher = updater.dispatcher
+webhook_info = bot.get_webhook_info()
+pending_update_count = webhook_info.pending_update_count
 
-def get_pending_updates():
-    # Get the last update ID
-    last_update_id = updater.dispatcher.bot.get_updates()[-1].update_id if updater.dispatcher.bot.get_updates() else 0
-    
-    # Get the number of pending updates
-    pending_updates = updater.dispatcher.bot.get_updates(offset=last_update_id + 1, timeout=10)
-    num_pending = len(pending_updates)
-    
-    print(f"Number of pending updates: {num_pending}")
-    
-    # If there are pending updates, print the first one
-    if num_pending > 0:
-        print(f"First pending update: {pending_updates[0]}")
-
-get_pending_updates()
+print(f"Number of pending updates: {pending_update_count}")
